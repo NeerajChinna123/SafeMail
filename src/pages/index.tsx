987532fc70c7ea310c384ad34b8f792780598274
Neeraj Baipureddy <neerajbaipureddy@Neerajs-MacBook-Pro.local>
2024-02-27 14:45:50 -0800

    minor fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
 
 import AfterEffectsVideo from "../components/AfterEffects";
 import { BannerType, FitZoneType } from "../../typings";
-import { EllipsisHorizontalCircleIcon } from '@heroicons/react/24/outline'
-import { UserPlusIcon, EnvelopeIcon, PhoneIcon, ChatBubbleLeftEllipsisIcon } from '@heroicons/react/24/solid'
+import { EllipsisHorizontalCircleIcon, PlusCircleIcon, PlusIcon } from '@heroicons/react/24/outline'
+import { UserPlusIcon, EnvelopeIcon, PhoneIcon, ChatBubbleLeftEllipsisIcon, ShieldExclamationIcon, BoltIcon, AtSymbolIcon } from '@heroicons/react/24/solid'
 import { useScroll } from "framer-motion";
 import { useState } from "react";
-import {motion} from 'framer-motion'
+import { motion } from 'framer-motion'
 
 
 export interface propsData {
@@ -14,7 +14,7 @@ export interface propsData {
 }
 
 const animationVariants = {
-  initial: {  opacity: 0 }, // Starting position off-screen (above)
+  initial: { opacity: 0 }, // Starting position off-screen (above)
   animate: { opacity: 1 }, // Ending position (on-screen)
 };
 
@@ -26,6 +26,8 @@ const animationTransition = {
 const Main = (props: propsData) => {
 
   const [currentNav, setCurrentNav] = useState('Emails')
+
+  const [currentNav1, setCurrentNav1] = useState('')
   return (
     //@ts-ignore
     <div className="relative">
@@ -64,6 +66,8 @@ const Main = (props: propsData) => {
                     <p className="text-gray-500 text-lg font-bold">Inbox</p>
                   </div>
 
+
+
                   <div onClick={() => setCurrentNav('Requests')} className={currentNav == 'Requests' ? "flex cursor-pointer flex-row space-x-4 p-3 items-center bg-black rounded-lg w-full transition-all transform duration-200 ease-in-out" : "flex cursor-pointer transition-all transform duration-200 ease-in-out p-3 w-full flex-row space-x-4 ml-2 items-center"}>
                     <UserPlusIcon className={currentNav == 'Requests' ? "h-7 w-7 text-white" : "h-7 w-7 text-black"} />
                     <p className={currentNav == 'Requests' ? "text-white text-lg font-semibold" : "text-black text-lg font-semibold"}>Requests</p>
@@ -73,6 +77,13 @@ const Main = (props: propsData) => {
                     <EnvelopeIcon className={currentNav == 'Emails' ? "h-7 w-7 text-white" : "h-7 w-7 text-black"} />
                     <p className={currentNav == 'Emails' ? "text-white text-lg font-semibold" : "text-black text-lg font-semibold"}>Emails</p>
                   </div>
+
+
+                  <div onClick={() => setCurrentNav('Phishing Attempt')} className={currentNav == 'Phishing Attempt' ? "flex cursor-pointer flex-row space-x-4 p-3 items-center bg-black rounded-lg w-full transition-all transform duration-200 ease-in-out" : "flex cursor-pointer transition-all transform duration-200 ease-in-out p-3 w-full flex-row space-x-4 ml-2 items-center"}>
+                    <ShieldExclamationIcon className={currentNav == 'Phishing Attempt' ? "h-7 w-7 text-white" : "h-7 w-7 text-black"} />
+                    <p className={currentNav == 'Phishing Attempt' ? "text-white text-lg font-semibold" : "text-black text-lg font-semibold"}>Phishing Attempt</p>
+                  </div>
+
 
 
                   <div onClick={() => setCurrentNav('Texts')} className={currentNav == 'Texts' ? "flex cursor-pointer flex-row space-x-4 p-3 items-center bg-black rounded-lg w-full transition-all transform duration-200 ease-in-out" : "flex cursor-pointer transition-all transform duration-200 ease-in-out p-3 w-full flex-row space-x-4 ml-2 items-center"}>
@@ -86,6 +97,29 @@ const Main = (props: propsData) => {
                   </div>
 
 
+                </div>
+
+
+                <div className="border-b border-gray-300 shadow shadow-gray-200 z-100 w-full  absolute top-[28.5rem]">
+
+
+
+                </div>
+
+                <div className=" flex flex-col space-y-2 pl-12 mt-[1.8rem] w-full">
+                  <div className="flex flex-row justify-between items-center">
+                    <p className="text-gray-500 text-lg font-bold">Categories</p>
+                    <PlusIcon className="h-6 w-6 text-gray-500" />
+                  </div>
+                  <div onClick={() => setCurrentNav1('Personal')} className={currentNav1 == 'Personal' ? "flex cursor-pointer flex-row space-x-4 p-3 items-center bg-black rounded-lg w-full transition-all transform duration-200 ease-in-out" : "flex cursor-pointer transition-all transform duration-200 ease-in-out p-3 w-full flex-row space-x-4 ml-2 items-center"}>
+                    <AtSymbolIcon className={currentNav1 == 'Personal' ? "h-7 w-7 text-white" : "h-7 w-7 text-black"} />
+                    <p className={currentNav1 == 'Personal' ? "text-white text-lg font-semibold" : "text-black text-lg font-semibold"}>Personal</p>
+                  </div>
+
+                  <div onClick={() => setCurrentNav1('Entertainment')} className={currentNav1 == 'Entertainment' ? "flex cursor-pointer flex-row space-x-4  items-center bg-black mt-[-1rem] rounded-lg w-full transition-all transform duration-200 ease-in-out px-3" : "flex cursor-pointer transition-all transform duration-200 ease-in-out px-3 w-full flex-row space-x-4 ml-2 items-center"}>
+                    <BoltIcon className={currentNav1 == 'Entertainment' ? "h-7 w-7 text-white" : "h-7 w-7 text-black"} />
+                    <p className={currentNav1 == 'Entertainment' ? "text-white text-lg font-semibold" : "text-black text-lg font-semibold"}>Entertainment</p>
+                  </div>
                 </div>
               </div>
             </div>
